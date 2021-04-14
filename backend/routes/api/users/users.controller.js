299c48d34657425d05155bbf72b70cdc9d62daa8
groveincharge <wdasent@yahoo.com>
2021-04-13 21:45:20 -0400

    working on passport authentication.

--- backend/routes/api/users/users.controller.js
+++ backend/routes/api/users/users.controller.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const passport = require('passport');
 const userService = require('./user.service');
 
 // routes
@@ -13,16 +15,94 @@ router.delete('/:id', _delete);
 
 module.exports = router;
 
-function authenticate(req, res, next) {
-    userService.authenticate(req.body)
-        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
-        .catch(err => next(err));
+async function authenticate(req, res, next) {
+    const {user} = req.body;
+
+
+    console.log('user', user);
+
+    await check('email', 'Invalid Credentials').isEmail().run(req);
+     await check('password', 'Invalid Credentials').isLength({ min: 6 })
+               .withMessage('password must be at least six chars long')
+               .matches(/\d/)
+               .withMessage('password must contain at least one number')
+               .run(req);
+       
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+     let errors = validationResult(req);
+     
+     if (!errors.isEmpty()) {
+        
+        return res.status(422).json({
+                             message: errors.errors.map((error, index) => {
+                               return `(${index + 1}) ${error.msg}.`
+                             })
+                           });      
+           }
+          else {
+            passport.authenticate('local', (err, passportUser, info) => {
+                console.log('Inside passport.authenticate() callback\n');
+                console.log(`passportUser inside passport.authenticate ${JSON.stringify(passportUser)}\n`)
+                console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}\n`);
+                console.log(`req.user: ${JSON.stringify(req.user)}\n`);
+            
+                req.login(passportUser, (err) => {
+                  
+                  console.log('Inside req.login() callback\n');
+                   console.log(`req.session.cookie ${JSON.stringify(req.session.cookie)}\n`);
+                  console.log(`req.session ${JSON.stringify(req.session)}\n`);
+                  console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}\n`);
+                  console.log(`passportUser inside req.login ${JSON.stringify(passportUser)}\n`);
+                  console.log(`req.session.id: ${JSON.stringify(req.session.id)}\n`);
+                  console.log(`req.user: ${JSON.stringify(req.user)}\n`);
+            
+                   if (req.isAuthenticated()) {
+                    
+                   }  
+                 else
+                    {
+                  res.status(401).json({
+                  isAuthenticated: req.isAuthenticated(),
+                  message: 'You must be registered to login.'
+                  })
+                 }
+              })
+            
+              })(req, res, next);
+
+             await userService.authenticate(req.user)
+              .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+              .catch(err => next(err));
+          }
+
 }
 
-function register(req, res, next) {
-    userService.create(req.body)
+ async function register(req, res, next) {
+     const {confirmPassword} = req.body;
+    await check('email', 'Invalid Credentials').isEmail().run(req)
+    await check('password', 'Invalid Credentials').isLength({ min: 6 })
+               .withMessage('password must be at least six chars long')
+               .matches(/\d/)
+              .withMessage('password must contain at least one number')
+              .equals(confirmPassword)
+              .withMessage('passwordConfirmation field must have the same value as the password field')
+              .run(req);
+  
+    const errors = validationResult(req);
+  
+    if (!errors.isEmpty()) {
+        
+       return res.status(422).json({
+                            message: errors.errors.map((error, index) => {
+                              return `(${index + 1}) ${error.msg}.`
+                            })
+                          });      
+          }
+        else {
+    await userService.create(req.body)
         .then(() => res.json({}))
         .catch(err => next(err));
+        }
 }
 
 function getAll(req, res, next) {
